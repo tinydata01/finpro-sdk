75ebc8b63e454f9d0277cdb128feb0c50175c9e8
manikanta <manikanta@divami.com>
2021-06-22 13:12:52 +0530

    added header

--- projects/onemoneyweb-sdk/src/lib/onemoneyweb-sdk.component.ts
+++ projects/onemoneyweb-sdk/src/lib/onemoneyweb-sdk.component.ts
@@ -2,17 +2,17 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'lib-onemoneyweb-sdk',
-  template: `
-  <app-linking-stepper></app-linking-stepper>
-  `,
-  styles: [
-  ]
+  templateUrl: "./onemoneyweb-sdk.component.html",
+  styleUrls: ["./onemoneyweb-sdk.component.scss"]
 })
 export class OnemoneywebSdkComponent implements OnInit {
-
+  isUserLoggedIn: boolean | undefined;
+  isLoggedIn: boolean = false;
+  companyLogo: any;
   constructor() { }
 
   ngOnInit(): void {
+    this.companyLogo = "../assets/images/pngs/HDFC-logo.png";
   }
 
 }
